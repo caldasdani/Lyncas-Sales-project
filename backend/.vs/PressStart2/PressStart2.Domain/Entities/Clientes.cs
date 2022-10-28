c9ba80e875498d369a58f7aa7b46b2da88438282
daniel-caldas <daniel.c@lyncas.net>
2022-10-28 12:41:51 -0300

    intro backend

---
+++ backend/.vs/PressStart2/PressStart2.Domain/Entities/Clientes.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class Clientes //a classe clientes possui 4 atributos
+{
+    public string? Nome { get; set; }
+    public string? Email { get; set; }
+    public double Cpf { get; set; }
+    public double Telefone { get; set; }
+	
+}
